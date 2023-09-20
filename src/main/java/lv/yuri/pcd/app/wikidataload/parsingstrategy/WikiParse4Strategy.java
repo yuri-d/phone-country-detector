bdf037261059f923de6235210428d974edbb30b9
yuri-d <yuri.sphere@gmail.com>
2023-09-20 13:37:01 +0300

    #3 - Strategy 4 and 5 implemented

--- src/main/java/lv/yuri/pcd/app/wikidataload/parsingstrategy/WikiParse4Strategy.java
+++ src/main/java/lv/yuri/pcd/app/wikidataload/parsingstrategy/WikiParse4Strategy.java
@@ -1,7 +1,7 @@
 package lv.yuri.pcd.app.wikidataload.parsingstrategy;
 
 import lombok.extern.slf4j.Slf4j;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -9,9 +9,43 @@ public class WikiParse4Strategy implements WikiParseStrategy {
 
     public static final String MATCH_PATTERN = "[0-9]+([(][,0-9]+[)])([,0-9 A-Za-z]+)";
 
+    private static final String RAW_CODE_SPLIT_PATTERN = "([(])|([)][,])";
+
+    private static final String RAW_CODE_REMOVE_CHARS_PATTERN = "[a-zA-Z]";
+
     @Override
     public List<Integer> parseCodes(String rawCodes) {
-        log.info("Parsing strategy not implemented for rawCodes:[{}]. Empty list will be returned.", rawCodes);
-        return Collections.emptyList();
+        log.info("Start parsing rawCodes rawCodes:[{}]", rawCodes);
+        List<String> rawCodeList = Arrays.stream(
+                        WikiParseStrategyUtils
+                                .normalize(rawCodes)
+                                .split(RAW_CODE_SPLIT_PATTERN))
+                .toList();
+        int mainCode = resolveMainCode(rawCodeList);
+        String subCode = resolveSubCode(rawCodeList);
+        int assignedCode = resolveAssignedCode(rawCodeList);
+
+        return Arrays.asList(
+                constructFullCode(mainCode, subCode),
+                assignedCode);
+    }
+
+    private static int resolveMainCode(List<String> rawCodeList) {
+        return Integer.parseInt(rawCodeList.get(0));
+    }
+
+    private static String resolveSubCode(List<String> rawCodeList) {
+        return rawCodeList.get(1)
+                .replace(")", "");
+    }
+
+    private static int resolveAssignedCode(List<String> rawCodeList) {
+        return Integer.parseInt(
+                rawCodeList.get(2)
+                        .replaceAll(RAW_CODE_REMOVE_CHARS_PATTERN, ""));
+    }
+
+    private static int constructFullCode(int mainCode, String subCode) {
+        return Integer.parseInt((mainCode+subCode));
     }
 }
--- src/main/java/lv/yuri/pcd/app/wikidataload/parsingstrategy/WikiParse5Strategy.java
+++ src/main/java/lv/yuri/pcd/app/wikidataload/parsingstrategy/WikiParse5Strategy.java
@@ -1,18 +1,54 @@
 package lv.yuri.pcd.app.wikidataload.parsingstrategy;
 
 import lombok.extern.slf4j.Slf4j;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 public class WikiParse5Strategy implements WikiParseStrategy {
 
     public static final String MATCH_PATTERN = "[0-9]+([(][,0-9]+[)])([(][,0-9 A-Za-z]+[)])";
+    private static final String RAW_CODE_SPLIT_PATTERN = "([(])|([)][,])";
+    private static final String RAW_CODE_REMOVE_CHARS_PATTERN = "[a-zA-Z)(]";
+    private static final String SUB_CODES_SPLIT_PATTERN = ",";
 
     @Override
     public List<Integer> parseCodes(String rawCodes) {
-        log.info("Parsing strategy not implemented for rawCodes:[{}]. Empty list will be returned.", rawCodes);
-        return Collections.emptyList();
+        log.info("Start parsing rawCodes rawCodes:[{}]", rawCodes);
+        List<String> rawCodeList = Arrays.stream(
+                        WikiParseStrategyUtils
+                                .normalize(rawCodes)
+                                .split(RAW_CODE_SPLIT_PATTERN))
+                .toList();
+        int mainCode = resolveMainCode(rawCodeList);
+        String subCodeList = resolveSubCodes(rawCodeList);
+        int assignedCode = resolveAssignedCode(rawCodeList);
+
+        List<Integer> fullCodeList = Stream.of(subCodeList.split(SUB_CODES_SPLIT_PATTERN))
+                .map(subCode -> constructFullCode(mainCode, subCode))
+                .collect(Collectors.toList());
+        fullCodeList.add(assignedCode);
+        return fullCodeList;
     }
 
+    private static int resolveMainCode(List<String> rawCodeList) {
+        return Integer.parseInt(rawCodeList.get(0));
+    }
+
+    private static String resolveSubCodes(List<String> rawCodeList) {
+        return rawCodeList.get(1)
+                .replace(")","");
+    }
+
+    private static int resolveAssignedCode(List<String> rawCodeList) {
+        return Integer.parseInt(
+                rawCodeList.get(2)
+                        .replaceAll(RAW_CODE_REMOVE_CHARS_PATTERN, ""));
+    }
+
+    private static int constructFullCode(int mainCode, String subCode) {
+        return Integer.parseInt((mainCode+subCode));
+    }
 }
